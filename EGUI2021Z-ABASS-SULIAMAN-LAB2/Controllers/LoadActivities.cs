f2ee0107ce8c3251d3dc7daade19acc33b8bb82e
Suliaman Abass <suliaman.abass46@gmail.com>
2021-12-09 00:19:17 +0100

    Data Structure foundation laid. Barebones json reading implemented.

---
+++ EGUI2021Z-ABASS-SULIAMAN-LAB2/Controllers/LoadActivities.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace EGUI2021Z_ABASS_SULIAMAN_LAB2.Controllers
+{
+    public class LoadActivities : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string LoadJson()
+        {
+            string filePath = @"C:\Users\Suliaman\source\repos\EGUI2021Z-ABASS-SULIAMAN-LAB2\EGUI2021Z-ABASS-SULIAMAN-LAB2\JsonFiles";
+            DataStructure.DataBase DB = new DataStructure.DataBase(filePath);
+
+
+            return  String.Join(",",DB.PrintList());
+        }
+         
+    }
+}
---
+++ EGUI2021Z-ABASS-SULIAMAN-LAB2/DataStructure/Month.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace EGUI2021Z_ABASS_SULIAMAN_LAB2.DataStructure
+{
+    public class Month
+    {   
+        [JsonProperty("entries")]
+        List<Entry> entries = new();
+
+        public string monthYear { get; set; }
+    }
+}
